81421a267553dfb85f1241c83e57c1bfc6d0da97
jianghai <jianghai1007@gmail.com>
2016-10-12 18:06:00 +0800

    update

--- index.js
+++ index.js
@@ -5,20 +5,39 @@ var mkdirp = require('mkdirp')
 var chalk = require('chalk')
 var ncp = require('ncp').ncp
 var spawn = require('cross-spawn')
+var pathExists = require('path-exists')
 
-var option = process.argv.slice(2)
-var baseDir = process.cwd()
-var projectName = option[0]
+function success(msg) {
+  console.log(chalk.green(`Success: ${msg}`))
+}
 
-if (projectName) {
+function error(msg) {
+  console.log(chalk.red(`Error: ${msg}`))
+}
+
+function run() {
+
+  var option = process.argv.slice(2)
+  var baseDir = process.cwd()
+  var projectName = option[0]
+
+  if (!projectName) return error('no project name provided, try `create-bfd-app <project>` instead.')
+
+  if (pathExists.sync(projectName)) return error(`folder ${projectName} exist.`)
+
+  if (process.version.slice(1).split('.')[0] < 6) return error(`Node version should be v6.x.`)
+
   mkdirp(projectName)
+  
   var dist = path.join(baseDir, projectName)
+  
   ncp(path.join(__dirname, 'template'), dist)
-  console.log(chalk.green('Created ' + dist))
+  success(`Created ${dist}.`)
+  
   spawn('npm', ['install'], {
     stdio: 'inherit',
     cwd: dist
   })
-} else {
-  console.log(chalk.red('Error: no project name provided, try `create-bfd-app <project>` instead.'))
 }
+
+run()
