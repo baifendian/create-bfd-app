287182a70a107d5ebcb9c5e2bfadbc8ebe50d07e
jianghai <jianghai1007@gmail.com>
2016-10-18 20:15:02 +0800

    fix npm ignore .gitignore

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
 var path = require('path')
-var mkdirp = require('mkdirp')
+var fs = require('fs')
 var chalk = require('chalk')
-var ncp = require('ncp').ncp
 var spawn = require('cross-spawn')
 var pathExists = require('path-exists')
 
@@ -13,6 +12,20 @@ function success(msg) {
 
 function error(msg) {
   console.log(chalk.red(`Error: ${msg}`))
+}
+
+function copyRecursiveSync(src, dest) {
+  var exists = fs.existsSync(src)
+  var stats = exists && fs.statSync(src)
+  var isDirectory = exists && stats.isDirectory()
+  if (exists && isDirectory) {
+    fs.mkdirSync(dest)
+    fs.readdirSync(src).forEach(function(childItemName) {
+      copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName))
+    })
+  } else {
+    fs.createReadStream(src).pipe(fs.createWriteStream(dest))
+  }
 }
 
 function run() {
@@ -27,15 +40,13 @@ function run() {
 
   if (process.version.slice(1).split('.')[0] < 6) return error(`Node version should be v6.x.`)
 
-  mkdirp(projectName)
-  
   var dist = path.join(baseDir, projectName)
-  
-  ncp(path.join(__dirname, 'template'), dist)
-  success(`Created ${dist}.`)
-  
-  console.log('Installing packages.... This might take a couple minutes.')
 
+  copyRecursiveSync(path.join(__dirname, 'template'), dist)
+  fs.renameSync(dist + '/gitignore', dist + '/.gitignore')
+
+  success(`Created ${dist}.`)
+  console.log('Installing packages.... This might take a couple minutes.')
   spawn('npm', ['install'], {
     stdio: 'inherit',
     cwd: dist
