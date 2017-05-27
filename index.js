68f3e6ca05bd2eb6c41b3385e6cfb95884d7a4ec
yuanchuan <yuanchuan23@gmail.com>
2017-05-27 12:04:30 +0800

    简化代码

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
 var path = require('path')
-var fs = require('fs')
+var fs = require('fs-extra')
 var chalk = require('chalk')
 var spawn = require('cross-spawn')
-var pathExists = require('path-exists')
 
 function success(msg) {
   console.log(chalk.green(`Success: ${msg}`))
@@ -14,43 +13,36 @@ function error(msg) {
   console.log(chalk.red(`Error: ${msg}`))
 }
 
-function copyRecursiveSync(src, dest) {
-  var exists = fs.existsSync(src)
-  var stats = exists && fs.statSync(src)
-  var isDirectory = exists && stats.isDirectory()
-  if (exists && isDirectory) {
-    fs.mkdirSync(dest)
-    fs.readdirSync(src).forEach(function(childItemName) {
-      copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName))
-    })
-  } else {
-    fs.createReadStream(src).pipe(fs.createWriteStream(dest))
-  }
-}
-
 function run() {
 
   var option = process.argv.slice(2)
   var baseDir = process.cwd()
   var projectName = option[0]
 
-  if (!projectName) return error('no project name provided, try `create-bfd-app <project>` instead.')
+  if (!projectName) {
+    return error('no project name provided, try `create-bfd-app <project>` instead.')
+  }
 
-  if (pathExists.sync(projectName)) return error(`folder ${projectName} exist.`)
+  if (fs.existsSync(projectName)) {
+    return error(`folder ${projectName} exist.`)
+  }
 
-  if (process.version.slice(1).split('.')[0] < 6) return error(`Node version should be v6.x.`)
+  if (process.version.slice(1).split('.')[0] < 6) {
+    return error(`Node version should be v6.x.`)
+  }
 
-  var dist = path.join(baseDir, projectName)
-
-  copyRecursiveSync(path.join(__dirname, 'template'), dist)
-  fs.renameSync(dist + '/gitignore', dist + '/.gitignore')
+  var src = path.join(__dirname, 'template')
+  var dist =  path.join(baseDir, projectName)
+  fs.copySync(src, dist)
 
   success(`Created ${dist}.`)
   console.log('Installing packages.... This might take a couple minutes.')
+
   spawn('npm', ['install'], {
     stdio: 'inherit',
     cwd: dist
   })
+
 }
 
 run()
